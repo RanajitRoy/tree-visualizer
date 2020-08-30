90d4f9829236be30c9d73cbc6b7b7ab487306401
Ranajit Roy <royranajit1@gmail.com>
2020-08-30 19:55:04 +0530

    added global variables

---
+++ src/globals.js
@@ -0,0 +1,13 @@
+var _globals_ = {
+    animationSpeed: 3,
+    canvasWidth: 1000,
+    canvasHeight: 500,
+    nodeRadius: 20,
+    nodeSpaceY: 60,
+    nodeSpaceX: 25,
+    ctx: null,
+    canvasPadding: null,
+    tree: null,
+};
+
+export default _globals_;
