c46bc668aef73bcb320b18398a767d1463006268
Ranajit Roy <royranajit1@gmail.com>
2020-10-10 23:15:42 +0530

    added home screen

--- src/index.js
+++ src/index.js
@@ -5,37 +5,30 @@ import BSTree from "./bst/BSTree";
 import { inorderOnNode } from "./bst/BSTTraversals";
 
 let canvasDiv = document.getElementById("canvasDiv");
-_globals_.canvasWidth = parseFloat(
-    window.getComputedStyle(canvasDiv).getPropertyValue("width")
-);
-_globals_.canvasHeight = parseFloat(
-    window.getComputedStyle(canvasDiv).getPropertyValue("height")
-);
 
 let canvas = document.getElementById("canvas");
-canvas.height = _globals_.canvasHeight;
-canvas.width = _globals_.canvasWidth;
+let canvasDivStyle = window.getComputedStyle(canvasDiv);
 _globals_.ctx = canvas.getContext("2d");
+
+function updateCanvasSize() {
+    _globals_.canvasWidth = parseFloat(
+        canvasDivStyle.getPropertyValue("width")
+    );
+    _globals_.canvasHeight = parseFloat(
+        canvasDivStyle.getPropertyValue("height")
+    );
+    canvas.height = _globals_.canvasHeight;
+    canvas.width = _globals_.canvasWidth;
+}
 
 window.addEventListener("resize", function () {
     canvas.width = 0;
     canvas.height = 0;
 
-    _globals_.canvasWidth = parseFloat(
-        window.getComputedStyle(canvasDiv).getPropertyValue("width")
-    );
-    _globals_.canvasHeight = parseFloat(
-        window.getComputedStyle(canvasDiv).getPropertyValue("height")
-    );
+    updateCanvasSize();
 
-    canvas.height = _globals_.canvasHeight;
-    canvas.width = _globals_.canvasWidth;
-
-    _globals_.tree.updateAllNodeUI();
+    _globals_.updateUI();
 });
-
-let bst = null;
-_globals_.tree = bst = new BSTree();
 
 let insertText = document.getElementById("insText");
 let deleteText = document.getElementById("delText");
@@ -46,8 +39,8 @@ insertButton.addEventListener("click", function (event) {
     event.target.disabled = true;
     let value = parseInt(insertText.value);
     if (!isNaN(value) && value > -10000 && value < 10000) {
-        bst.insert(value);
-        bst.updateAllNodeUI();
+        _globals_.tree.insert(value);
+        _globals_.updateUI();
     }
 
     event.target.disabled = false;
@@ -57,11 +50,37 @@ deleteButton.addEventListener("click", function (event) {
     event.target.disabled = true;
     let value = parseInt(deleteText.value);
     if (!isNaN(value) && value > -10000 && value < 10000) {
-        bst.delete(value);
-        bst.updateAllNodeUI();
+        _globals_.tree.delete(value);
+        _globals_.updateUI();
     }
     event.target.disabled = false;
 });
+
+let variable = document.getElementById("variable");
+let homevariable = document.getElementById("homevariable");
+
+let logo = document.getElementById("logoheader");
+logo.addEventListener("click", function (event) {
+    _globals_.tree = null;
+    _globals_.updateUIHook = null;
+    variable.className = "variableNone";
+    homevariable.className = "homeVariable";
+});
+
+let bstNav = document.getElementById("bstnav");
+let bstHome = document.getElementById("bsthome");
+let bstHandler = function (event) {
+    _globals_.tree = new BSTree();
+    _globals_.updateUIHook = _globals_.tree.updateAllNodeUI.bind(
+        _globals_.tree
+    );
+    variable.className = "variable";
+    homevariable.className = "homeVariableNone";
+
+    updateCanvasSize();
+};
+bstNav.addEventListener("click", bstHandler);
+bstHome.addEventListener("click", bstHandler);
 
 // bst.insert(60);
 // bst.insert(201);
